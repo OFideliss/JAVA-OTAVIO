e6f04cbbe3ccca1303b801f109764faaee78e3ff
OFideliss <otavio-fidelis@outlook.com>
2023-02-13 22:00:25 -0300

    13/02/2023

--- CalculadoraDescontos/CalculadoraDescontos.java
+++ CalculadoraDescontos/CalculadoraDescontos.java
@@ -1,9 +1,27 @@
-package CalculadoraDescontos;
+package CalculadoraDescontos; // mesmo nome da pasta 
 
 /**
  * CalculadoraDescontos
  */
-public class CalculadoraDescontos {
+import java.util.Scanner;
 
-    
+public class CalculadoraDescontos { // "CLASSE" mesmo nome do arquivo .java
+    public void calcular20() {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Informe o preço do produto");
+        double precoProduto;
+        precoProduto = sc.nextDouble();
+        final int desconto20 = 20;
+        double precoComDesconto = precoProduto - (precoProduto * desconto20 / 100);
+        System.out.println("O Preço com Desconto é " + precoComDesconto + " reais");
+        sc.close();
+    }
+
+    public void calcular30() {
+        double precoProduto;
+        precoProduto = 200;
+        final int desconto30 = 30;
+        double precoComDesconto = precoProduto - (precoProduto * desconto30 / 100);
+        System.out.println("O Preço com Desconto é " + precoComDesconto + " reais");
+    }
 }
