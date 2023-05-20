a040725ce8e3789989f4e78bf447632850aa96b9
OFideliss <otavio-fidelis@outlook.com>
2023-05-19 21:33:45 -0300

    19/05/2023

--- IteracaoWhile/App.java
+++ IteracaoWhile/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) { // chamar o "main"
         ListaExtra objeto1 = new ListaExtra();
+        ListaExtra objeto1 = new ListaExtra();
         //objeto1.exercicio1();
         //objeto1.exercicio2();
         // objeto1.exercicio3();
@@ -14,7 +15,7 @@ public class App {
          //objeto1.exercicio8();   
          //objeto1.exercicio9();
          //objeto1.exercicio10();
-         objeto1.exercicio11();   
+         //objeto1.exercicio11();   
          //objeto1.exercicio12();
         }
 }
--- Matriz/App.java
+++ Matriz/App.java
@@ -10,10 +10,10 @@ public class App {
 
         // obj1.exercicio1();
         //obj1.exercicio2();
-        //obj1.exercicio3();
+        obj1.exercicio3();
         //obj1.exercicio4();
          //obj1.exercicio5();
-         obj1.exercicio6();
+         //obj1.exercicio6();
 
     }
 
