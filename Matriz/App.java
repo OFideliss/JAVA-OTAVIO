20508805c11f9eebfa901e54074a965f8850e8a0
OFideliss <otavio-fidelis@outlook.com>
2023-06-10 19:31:08 -0300

    10/06/23

--- Matriz/App.java
+++ Matriz/App.java
@@ -4,16 +4,16 @@ public class App {
     public static void main(String[] args) {
         MatrizExemplo obj = new MatrizExemplo();
         ListaExtra obj1 = new ListaExtra();
-        // obj.exemplo1();
+        obj.exemplo1();
         // obj.exercicio1();
         // obj.exercicio2();
 
-        // obj1.exercicio1();
-        //obj1.exercicio2();
-        //obj1.exercicio3();
-        //obj1.exercicio4();
-         //obj1.exercicio5();
-         obj1.exercicio6();
+        obj1.exercicio1();
+        // obj1.exercicio2();
+        // obj1.exercicio3();
+        // obj1.exercicio4();
+        // obj1.exercicio5();
+        // obj1.exercicio6();
 
     }
 
