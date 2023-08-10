318cba2e5513e7c126b40bef10d009a7be183e5b
OFideliss <otavio-fidelis@outlook.com>
2023-08-10 20:16:28 -0300

    10/08/23

---
+++ EstudoArrayList/ExemploLinkedList.java
@@ -0,0 +1,13 @@
+package EstudoArrayList;
+
+import java.util.LinkedList;
+
+public class ExemploLinkedList {
+    //Atributos
+    LinkedList <String> pessoas = new LinkedList<>();
+    //
+    //método
+    public void teste() {
+        
+    }
+}
