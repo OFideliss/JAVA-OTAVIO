839f82771fd40da128ece7378032be83203f9fd7
OFideliss <otavio-fidelis@outlook.com>
2023-08-09 21:50:29 -0300

    09/08/23

---
+++ EstudoArrayList/ExemploArrayList.java
@@ -0,0 +1,34 @@
+package EstudoArrayList;
+
+import java.util.ArrayList;
+
+public class ExemploArrayList {
+    // atributo
+    // Declarando o ArrayList do tipo String
+    ArrayList<String> carros = new ArrayList<>();
+    //
+    // método
+    public void teste() {
+        // adicionar valores ao ArrayList
+        carros.add("Uno");
+        carros.add("Chevette");
+        carros.add("Celta");
+        carros.add("Monza");
+        carros.add("Gol");
+        // Imprimir toda a lista
+        System.out.println(carros);
+        // Imprimir valor do index 4 (Quinta posição)
+        System.out.println(carros.get(4));
+        //Percorrendo a lista com o FOR-I
+        for (int i = 0; i < carros.size(); i++) {
+            System.out.println(carros.get(i));
+        }
+        //Percorrendo a lista com FOR-EACH
+        for (String i : carros) {
+            System.out.println(i);
+        }
+        // Apagar a lista
+        carros.clear();
+        System.out.println(carros);
+    }
+}
