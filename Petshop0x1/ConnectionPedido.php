2d9c20aae84ccbca122338745c13b24a6edb5f97
OFideliss <otavio-fidelis@outlook.com>
2023-12-07 21:57:18 -0300

    07/12/23

---
+++ Petshop0x1/ConnectionPedido.php
@@ -0,0 +1,33 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$DB = "loja";
+
+$conn = mysqli_connect($host, $username, $password) or die("Impossível conectar ao banco.");
+@mysqli_select_db($conn, $DB) or die("Impossível conectar ao banco");
+
+$query = "SELECT * FROM pedidos";
+$result = mysqli_query($conn, $query) or die("Impossível executar a query");
+
+$pedidos = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $numPedido = $row['numPedido'];
+
+    if (!array_key_exists($numPedido, $pedidos)) {
+        $pedidos[$numPedido] = array(
+            'info' => $row,
+            'itens' => array(),
+            'total' => 0 // Adicionando o campo total para cada pedido
+        );
+    }
+
+    $pedidos[$numPedido]['itens'][] = $row;
+    $pedidos[$numPedido]['total'] += $row['valor']; // Somando o valor ao total do pedido
+}
+
+mysqli_close($conn);
+
+?>
+<
